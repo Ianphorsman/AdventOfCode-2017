a41e444e9ccf17c6f4cb02206f49ae9c82628059
Ian Horsman <ianphorsman@gmail.com>
2017-12-03 22:58:53 -0500

    why sum when reduce can do that for you

--- day_1.py
+++ day_1.py
@@ -4,7 +4,7 @@ def part_1():
     with open('day_1_data_1') as file:
         data = file.read()
         data = data + data[0]
-        return sum(reduce(lambda x,y: x + [int(data[y])] if data[y] == data[y+1] else x, range(len(data)-1), [0]))
+        return reduce(lambda x,y: x + int(data[y]) if data[y] == data[y+1] else x, range(len(data)-1), 0)
 
 print(part_1())
 
@@ -12,6 +12,6 @@ def part_2():
     with open('day_1_data_2') as file:
         data = file.read()
         size = len(data)
-        return sum(reduce(lambda x,y: x + [int(data[y])] if data[y] == data[(y+size//2)%size] else x, range(size), [0]))
+        return reduce(lambda x,y: x + int(data[y]) if data[y] == data[(y+size//2)%size] else x, range(size), 0)
 
 print(part_2())
---
+++ day_3.py
@@ -0,0 +1,15 @@
+
+def part_1():
+    with open('day_3_data_1') as file:
+        data = file.read()
+
+        return data
+
+print(part_1())
+
+def part_2():
+    with open('day_3_data_1') as file:
+        data = file.read()
+        return data
+
+#print(part_2())
