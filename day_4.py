7727030e3ccba4c2ab79e9c2ed7ac12a6f305612
Ian Horsman <ianphorsman@gmail.com>
2017-12-04 01:01:12 -0500

    Day 4 part 1

---
+++ day_4.py
@@ -0,0 +1,16 @@
+
+def part_1():
+    with open('day_4_data_1') as file:
+        data = file.readlines()
+        data = [line.replace('\n', '').split(' ') for line in data]
+        return len(list(filter(lambda x: len(x) == len(set(x)), data)))
+
+print(part_1())
+
+
+def part_2():
+    with open('day_4_data_1') as file:
+        data = file.read()
+        return data
+
+#print(part_2())
