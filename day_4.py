b7890f0f760ddb1706aeb30db2129cb6c47e8e7f
Ian Horsman <ianphorsman@gmail.com>
2017-12-04 01:16:57 -0500

    Day 4 part 2

--- day_4.py
+++ day_4.py
@@ -10,7 +10,8 @@ print(part_1())
 
 def part_2():
     with open('day_4_data_1') as file:
-        data = file.read()
-        return data
+        data = file.readlines()
+        data = [line.replace('\n', '').split(' ') for line in data]
+        return len(list(filter(lambda x: len(x) == len(set([''.join(sorted(phrase)) for phrase in x])), data)))
 
-#print(part_2())
+print(part_2())
