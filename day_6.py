54ce86e92c63bf6f74efd74d979f6227c1891765
Ian Horsman <ianphorsman@gmail.com>
2017-12-11 21:10:07 -0500

    Removed first attempt at solution

--- day_6.py
+++ day_6.py
@@ -46,33 +46,3 @@ def distribute_blocks(data):
 print(part_1())
 print(part_2())
 
-'''
-def part_1():
-    with open('day_6_data_1') as file:
-        data = list(map(int, file.read().split()))
-        data = list(map(list, zip(range(len(data)), data)))
-        distributions = [data]
-        for count in range(50):
-            sorted_data = distributions[-1]
-            #print(len(distributions))
-            sorted_data.sort(key=lambda tup: tup[1])
-            span = min(len(sorted_data), sorted_data[-1][1])
-            for i in range(len(sorted_data)):
-                if i < span:
-                    sorted_data[i][1] += 1
-            sorted_data[-1][1] -= span
-            unzipped_data = sorted_data
-            unzipped_data.sort(key=lambda tup: tup[0])
-            print(count, unzipped_data)
-            distributions.append(unzipped_data[:])
-
-        return distributions
-
-for l in part_1():
-    print([tup[1] for tup in l])
-
-def part_2():
-    with open('day_6_data_1') as file:
-        data = file.read()
-        return data
-'''
