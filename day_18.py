421bcd643a40c0424c6ffc864566648166008f32
Ian Horsman <ianphorsman@gmail.com>
2017-12-18 22:26:25 -0500

    part 2"

--- day_18.py
+++ day_18.py
@@ -1,13 +1,18 @@
-from collections import defaultdict, deque
+from collections import defaultdict
 
 
 class Register(object):
 
-    def __init__(self, data, registers):
+    def __init__(self, id, data, registers):
+        self.id = id
         self.data = data
         self.op_index = 0
         self.registers = registers
-        self.played_sounds = deque()
+        self.registers['p'] = self.id
+        self.coupled_register = None
+        self.send_count = 0
+        self.sends = []
+        self.done = False
 
 
     def inc(func):
@@ -16,13 +21,34 @@ class Register(object):
             func(self, *args)
         return wrapper
 
+    # part 1
     def run(self):
-        while 0 <= self.op_index < len(self.data) - 1:
+        while 0 <= self.op_index < len(self.data):
             self.eval_command()
 
+    def can_run(self):
+        if self.done:
+            return False
+        else:
+            try:
+                rcv = self.data[self.op_index][0] == 'rcv'
+                if rcv:
+                    return len(self.coupled_register.sends) > 0
+            except IndexError:
+                return False
+            return True
+
     def eval_command(self):
-        op = self.data[self.op_index]
-        getattr(self, op[0])(*op[1:])
+        if not self.can_run():
+            return
+        else:
+            try:
+                op = self.data[self.op_index]
+            except IndexError:
+                self.done = True
+                print("Done")
+                return
+            getattr(self, op[0])(*op[1:])
 
     def eval_pointer(self, y):
         if str(y).isalpha():
@@ -32,7 +58,8 @@ class Register(object):
 
     @inc
     def snd(self, x):
-        self.played_sounds.append(self.registers[x])
+        self.sends.append(self.eval_pointer(x))
+        self.send_count += 1
 
     @inc
     def set(self, x, y):
@@ -52,9 +79,9 @@ class Register(object):
 
     @inc
     def rcv(self, x):
-        #print(self.eval_pointer(x))
-        if not self.eval_pointer(x) == 0:
-            self.played_sounds.rotate()
+        #if not self.eval_pointer(x) == 0:
+        #if len(self.coupled_register.sends) > 0:
+        self.registers[x] = self.coupled_register.sends.pop(0)
 
     def jgz(self, x, y):
         if not self.eval_pointer(x) == 0:
@@ -69,15 +96,24 @@ def part_1():
         data = [line.split() for line in file.readlines()]
         registers = Register(data, defaultdict(int))
         registers.run()
-        return registers.played_sounds[0]
+        return #registers.played_sounds[0]
 
 
 
 def part_2():
     with open('day_18_data_1') as file:
         data = [line.split() for line in file.readlines()]
+        p0 = Register(0, data, defaultdict(int))
+        p1 = Register(1, data, defaultdict(int))
+        p0.coupled_register = p1
+        p1.coupled_register = p0
 
-        return data
+        while True:
+            p0.eval_command()
+            p1.eval_command()
+            if not p0.can_run() and not p1.can_run():
+                break
+        return p1.send_count
 
 
-print(part_1())
+print(part_2())
